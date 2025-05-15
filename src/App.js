9d1c7259224ad105c42abdfbddebf49575cc1822
momomomomomomomomomomomomo <142402844+momomomomomomomomomomomomo@users.noreply.github.com>
2025-05-15 06:34:03 +0300

    final vesrion

--- src/App.js
+++ src/App.js
@@ -8,36 +8,48 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
   const handlePrevious = function () {
     setStep((step) => (step > 1 ? step - 1 : step));
   };
   const handleNext = function () {
     setStep((step) => (step < messages.length ? step + 1 : step));
   };
+  const handleClose = function () {
+    setIsOpen((is) => !is);
+  };
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
+    <>
+      <div className="close" onClick={handleClose}>
+        {isOpen ? "\u00D7" : "+"}
       </div>
-      <div className="message">
-        Step {step}: {messages[step - 1]}
-      </div>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#FFF" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#FFF" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <div className="message">
+            Step {step}: {messages[step - 1]}
+          </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#FFF" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#FFF" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
